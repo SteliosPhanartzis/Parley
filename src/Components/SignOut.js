84ed39d7a9d5cd8c491edb0b5d64fd03a326af8f
SteliosPhanartzis <stelios.phan@gmail.com>
2020-12-13 02:38:44 -0500

    Clean slate

--- src/Components/SignOut.js
+++
@@ -1,8 +0,0 @@
-function SignOut(props) {
-    const auth = props.auth;
-    return auth.currentUser && (
-        <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-    );
-  }
-
-export default SignOut;
